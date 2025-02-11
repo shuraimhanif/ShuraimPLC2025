b1d719558875011a6aa2830c7d1b7651dfb2b692
shuraim hanif <shuraimhanif99@gmail.com>
2025-02-11 20:01:59 +0000

    task 2 sgn function using conditionals

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,6 +3,7 @@ myFunc1 x = x*10
 myFunc2 x = x*2 -- functions cannot begin with capitals (was 'MyFunc2')
 thirdFunc = "Hello, this is 3rd Func" -- variables cannot start with numbers (was '3rdFunc')
 
+
 main =
     do
     print (myFunc1 5)
--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,15 @@
+onePlusone = 
+    print("1+1=2")
+
+--conditionals--
+sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+    input <- getLine --gets number from input--
+    let x = (read input :: Int) --assigns number to variable x--
+    putStrLn(show (sgn (x))) --executes sgn function on number--
